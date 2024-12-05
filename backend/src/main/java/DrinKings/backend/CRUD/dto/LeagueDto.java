9727e2accdad58f638991a88a7d696c1402d1452
Pablo Sánchez <pablo.sanchez@delonia.com>
2024-12-05 20:19:54 +0100

    update 05/12/2024/20:19

--- backend/src/main/java/DrinKings/backend/CRUD/dto/LeagueDto.java
+++ backend/src/main/java/DrinKings/backend/CRUD/dto/LeagueDto.java
@@ -1,5 +1,31 @@
 package DrinKings.backend.CRUD.dto;
 
+import DrinKings.backend.CRUD.entity.League;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+// @AllArgsConstructor
 public class LeagueDto {
+    private String name;
+    private String description;
+
+    // public League toEntity() {
+    // return League.builder()
+    // .name(name)
+    // .description(description)
+    // .build();
+    // }
+
+    @Builder
+    public LeagueDto(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 
 }
