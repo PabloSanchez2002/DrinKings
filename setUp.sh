8a887661d581dccd4036e7b0b5e6f3b32b611587
Pablo Sánchez <pablo.sanchez@delonia.com>
2024-12-13 16:19:20 +0100

    empezando front

--- setUp.sh
+++ setUp.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-# Set up the environment
+# Set up the environment back
 # java 21.0.5
-# Gradle 4.4.1
 # ./gradlew bootRun
 # ./gradlew clean build --refresh-dependencies
+# ./gradlew bootJar
+# java -jar build/libs/backend-0.0.1-SNAPSHOT.jar
+
+# Set up the environmentfront
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+# nvm install 22
+# nvm use 22
+# npm create vite@latest DrinKings-front
+# npm install
+# npm install -g cordova
+# npm run dev
+# npm run build
+# npm run preview
+
